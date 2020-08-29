d45ed3f98e8f7eff3af8e8639978abf918e1df7e
kylebell3 <bellky@oregonstate.edu>
2020-08-28 19:24:09 -0700

    trying to start with a drawn square as a character and making that move. it can move but in the next frame it is kicked back to default position.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+
 pygame.init()
 
 pygame.display.set_caption("An Adventure")
@@ -14,15 +16,61 @@ green = (125, 222, 146)
 blue_green = (46, 191, 165)
 
 while True:
+    # player starting coordinates and size
+    player_x = 375
+    player_y = 500
+    player_w = 50
+    player_h = 50
+
+    # clock
+    clock = pygame.time.Clock()
+
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    screen.fill(yellow)
+    
+
+
+    ##########
+    # Movement
+    ###########
+    
+        keys = pygame.key.get_pressed()  # checks what keys are pressed
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                player_x += -5
+
+        #     elif event.key == pygame.K_RIGHT:
+        #         player_x += 5
+
+        #     elif event.key == pygame.K_p:
+        #         pause = True
+        #         paused()
+
+        # # if a key is lifed while another is still held down     
+        # if event.type == pygame.KEYUP:
+            
+        #     if (event.key == pygame.K_RIGHT) and keys[pygame.K_LEFT]:
+        #         player_x += -5
+
+        #     elif (event.key == pygame.K_LEFT) and keys[pygame.K_RIGHT]:
+        #         player_x += 5 
+                
+        #     else:
+        #         player_x += 0
+
+    print(player_x)
+    screen.fill(yellow) 
+
+    pygame.draw.rect(screen, blue, [player_x, player_y, player_w, player_h])
 
     pygame.display.update()
-
+    clock.tick(60)
 
 pygame.quit()
 quit()
