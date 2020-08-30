3a85c44e5f3ed925043684dc709051ea336ae4f1
kylebell3 <bellky@oregonstate.edu>
2020-08-29 21:50:13 -0700

    created a sword that appears when spacebar is pressed

--- main.py
+++ main.py
@@ -18,8 +18,12 @@ blue_green = (46, 191, 165)
 # clock
 clock = pygame.time.Clock()
 
-def game_loop():
 
+sword = False  # sword starts put away
+
+
+def game_loop():
+    # player starting coordinates and size
     player_x = 375
     player_y = 500
     player_w = 50
@@ -27,9 +31,12 @@ def game_loop():
     player_speed = 3
     x = 0       # used to change x value
     y = 0       # used to change y value
+    sword_counter = 0
 
     while True:
-        # player starting coordinates and size
+
+        screen.fill(yellow) 
+        global sword
 
         for event in pygame.event.get():
             keys = pygame.key.get_pressed()  # checks what keys are pressed
@@ -51,8 +58,13 @@ def game_loop():
                     if event.key == pygame.K_DOWN:
                         y = player_speed
 
-                    elif event.key == pygame.K_UP:
+                    if event.key == pygame.K_UP:
                         y = -player_speed
+
+                    # attack with sword
+                    if event.key == pygame.K_SPACE:
+                        sword = True
+                        print('what')
 
             # if a key is lifed while another is still held down     
             elif event.type == pygame.KEYUP:
@@ -93,13 +105,24 @@ def game_loop():
             if y < 0:
                 y = 0
 
-
-        player_x += x
+        # set new position based on movement
+        player_x += x  
         player_y += y
 
-        screen.fill(yellow) 
+
+        # sword will swing for ~.8 seconds
+        if sword_counter >= 50:
+            sword = False
+            sword_counter = 0
+
+        if sword:
+            # draw sword
+            pygame.draw.rect(screen, blue_green, [(player_x + 35), (player_y - 55), 9, 60])
+            pygame.draw.rect(screen, blue_green, [(player_x + 20), (player_y - 20), 37, 8])
+            sword_counter += 1
 
         pygame.draw.rect(screen, blue, [player_x, player_y, player_w, player_h])
+
 
         # pygame.draw.rect(screen, blue_green, [500, 0, 2, 1000]) # line long screen
 
